aa7852c630916d2557265b3ae253c310d7a2b2ad
shishikura <ue_wo@yahoo.co.jp>
2021-08-26 10:21:31 +0900

    簡単な例を実装

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -20,15 +20,104 @@ namespace View
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public ViewModel Sample1 { get; } = new ViewModel();
+		public ViewModel MainViewModel { get; private set; } = new NormalDistributionViewModel();
+
+		public List<KeyValuePair<Distribution, string>> Distributions { get; } = new List<KeyValuePair<Distribution, string>>()
+		{
+			new KeyValuePair<Distribution, string>(Distribution.Normal, "ガウス分布（正規分布）"),
+			new KeyValuePair<Distribution, string>(Distribution.Gamma, "ガンマ分布"),
+			new KeyValuePair<Distribution, string>(Distribution.StudentT, "スチューデントのt分布"),
+			new KeyValuePair<Distribution, string>(Distribution.Learning, "学習テスト"),
+		};
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
 			DataContext = this;
-
-			Sample1.Init();
+			DistributionComboBox.SelectedIndex = 0;
 		}
+
+		private void DistributionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var distribution = (Distribution)DistributionComboBox.SelectedValue;
+			SetTextBoxVisible((distribution == Distribution.Learning) ? Visibility.Visible : Visibility.Hidden);
+
+			MainViewModel = distribution switch
+			{
+				Distribution.Normal => new NormalDistributionViewModel(),
+				Distribution.Gamma => new GammaDistributionViewModel(),
+				Distribution.StudentT => new StudentTDistributionViewModel(),
+				Distribution.Learning => new LearningViewModel(),
+				_ => throw new Exception(),
+			};
+
+			if (distribution == Distribution.Learning)
+			{
+				SetTrueDistribution();
+			}
+
+			MainPlotView.Model = MainViewModel.Model;
+			MainPlotView.InvalidatePlot(true);
+			MainViewModel.Model.InvalidatePlot(true);
+		}
+
+		private void SetTextBoxVisible(Visibility visibility)
+		{
+			TrueDistibutionLabel.Visibility = visibility;
+			MeanLabel.Visibility = visibility;
+			MeanTextBox.Visibility = visibility;
+			PrecisionLabel.Visibility = visibility;
+			PrecisionTextBox.Visibility = visibility;
+			TrueDistributionButton.Visibility = visibility;
+			PriorDistributionLabel.Visibility = visibility;
+			MLabel.Visibility = visibility;
+			MTextBox.Visibility = visibility;
+			BetaLabel.Visibility = visibility;
+			BetaTextBox.Visibility = visibility;
+			ALabel.Visibility = visibility;
+			ATextBox.Visibility = visibility;
+			BLabel.Visibility = visibility;
+			BTextBox.Visibility = visibility;
+			SampleLabel.Visibility = visibility;
+			SampleTextBox.Visibility = visibility;
+			LearningButton.Visibility = visibility;
+			SampleViewTextBox.Visibility = visibility;
+		}
+
+		private void TrueDistributionButton_Click(object sender, RoutedEventArgs e)
+		{
+			SetTrueDistribution();
+		}
+
+		private void SetTrueDistribution()
+		{
+			var learning = (LearningViewModel)MainViewModel;
+			learning.SetTrueDistribution(double.Parse(MeanTextBox.Text), double.Parse(PrecisionTextBox.Text));
+		}
+
+		private void LearningButton_Click(object sender, RoutedEventArgs e)
+		{
+			Learning();
+		}
+
+		private void Learning()
+		{
+			var learning = (LearningViewModel)MainViewModel;
+			learning.Init(double.Parse(MTextBox.Text), double.Parse(BetaTextBox.Text), double.Parse(ATextBox.Text), double.Parse(BTextBox.Text));
+			var sample = learning.Samples(int.Parse(SampleTextBox.Text));
+			SampleViewTextBox.Text = string.Join(Environment.NewLine, sample);
+			learning.Learn(sample);
+		}
+
+		
+	}
+
+	public enum Distribution
+	{
+		Normal,
+		Gamma,
+		StudentT,
+		Learning,
 	}
 }
--- View/ViewModel.cs
+++ View/ViewModel.cs
@@ -5,69 +5,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OxyPlot;
+using OxyPlot.Series;
+using MathNet.Numerics.Statistics;
+using MathNet.Numerics.Distributions;
 
 namespace View
 {
-    public class ViewModel
+    public interface ViewModel
     {
+        public PlotModel Model { get; }
+    }
 
-        public PlotModel Model { get; } = new PlotModel();
-        public PlotController Controller { get; } = new PlotController();
-
-        public OxyPlot.Axes.TimeSpanAxis X { get; } = new OxyPlot.Axes.TimeSpanAxis();
-        public OxyPlot.Axes.LinearAxis Y { get; } = new OxyPlot.Axes.LinearAxis();
-        public OxyPlot.Series.LineSeries LineSeries { get; private set; }
-        public OxyPlot.Series.FunctionSeries FunctionSeries { get; private set; }
-
-        public ObservableCollection<TestData> Samples { get; private set; }
-
-        public void Init()
+    public class NormalDistributionViewModel : ViewModel
+    {
+        public PlotModel Model { get; } = new PlotModel { Title = "ガウス分布" };
+        public NormalDistributionViewModel()
         {
-            Samples = new ObservableCollection<TestData>
-            {
-                new TestData{ Time= new TimeSpan(0,0,0), Value=0, Tag="A" },
-                new TestData{ Time= new TimeSpan(0,0,1), Value=2, Tag="B" },
-                new TestData{ Time= new TimeSpan(0,0,2), Value=4, Tag="C" },
-                new TestData{ Time= new TimeSpan(0,0,3), Value=6, Tag="D" },
-                new TestData{ Time= new TimeSpan(0,0,4), Value=0, Tag="E" },
-                new TestData{ Time= new TimeSpan(0,0,5), Value=2, Tag="F" },
-            };
+            Model.Series.Add(new FunctionSeries(Normal.WithMeanPrecision(10.0, 2.5).Density, -20.0, 20.0, 0.01, "μ=10, λ=2.5"));
+            Model.Series.Add(new FunctionSeries(Normal.WithMeanPrecision(0.0, 0.1).Density, -20.0, 20.0, 0.01, "μ=0, λ=0.1"));
+            Model.Series.Add(new FunctionSeries(Normal.WithMeanPrecision(-10.0, 0.8).Density, -20.0, 20.0, 0.01, "μ=-10, λ=0.8"));
+        }
+    }
 
-            Model.Title = "PlotView";
+    public class GammaDistributionViewModel : ViewModel
+    {
+        public PlotModel Model { get; } = new PlotModel { Title = "ガンマ分布" };
+        public GammaDistributionViewModel()
+        {
+            Model.Series.Add(new FunctionSeries(Gamma.WithShapeRate(1.0, 1.0).Density, 0.0, 20.0, 0.01, "a=1.0, b=1.0"));
+            Model.Series.Add(new FunctionSeries(Gamma.WithShapeRate(1.0, 2.0).Density, 0.0, 20.0, 0.01, "a=1.0, b=2.0"));
+            Model.Series.Add(new FunctionSeries(Gamma.WithShapeRate(2.0, 1.0).Density, 0.0, 20.0, 0.01, "a=2.0, b=1.0"));
+            Model.Series.Add(new FunctionSeries(Gamma.WithShapeRate(1.0, 2.0).Density, 0.0, 20.0, 0.01, "a=1.0, b=2.0"));
+            Model.Series.Add(new FunctionSeries(Gamma.WithShapeRate(9.0, 0.5).Density, 0.0, 20.0, 0.01, "a=9.0, b=0.5"));
+        }
+    }
 
-            // 軸の初期化
-            X.Position = OxyPlot.Axes.AxisPosition.Bottom;
-            Y.Position = OxyPlot.Axes.AxisPosition.Left;
+    public class StudentTDistributionViewModel : ViewModel
+    {
+        public PlotModel Model { get; } = new PlotModel { Title = "スチューデントのt分布" };
+        public StudentTDistributionViewModel()
+        {
+            Model.Series.Add(new FunctionSeries(new StudentT(0.0, 1.0, 1.0).Density, -20.0, 20.0, 0.01, "μ=0.0, λ=1.0, ν=1.0"));
+            Model.Series.Add(new FunctionSeries(new StudentT(10.0, 1.0, 1.0).Density, -20.0, 20.0, 0.01, "μ=10.0, λ=1.0, ν=1.0"));
+            Model.Series.Add(new FunctionSeries(new StudentT(0.0, 3.0, 1.0).Density, -20.0, 20.0, 0.01, "μ=0.0, λ=3.0, ν=1.0"));
+            Model.Series.Add(new FunctionSeries(new StudentT(0.0, 3.0, 3.0).Density, -20.0, 20.0, 0.01, "μ=0.0, λ=3.0, ν=3.0"));
+            Model.Series.Add(new FunctionSeries(new StudentT(0.0, 3.0, Double.MaxValue).Density, -20.0, 20.0, 0.01, "μ=0.0, λ=3.0, ν=Double.MaxValue"));
+        }
+    }
 
-            // 線グラフ
-            LineSeries = new OxyPlot.Series.LineSeries();
-            LineSeries.Title = "Custom";
-            LineSeries.ItemsSource = Samples;
-            LineSeries.DataFieldX = nameof(TestData.Time);
-            LineSeries.DataFieldY = nameof(TestData.Value);
+    public class LearningViewModel : ViewModel
+	{
+        public PlotModel Model { get; } = new PlotModel { Title = "学習" };
+        private Normal TrueDistribution { get; set; } = new Normal();
 
-            var a = 1;
-            var b = 2;
-
-            // 関数グラフ
-            FunctionSeries = new OxyPlot.Series.FunctionSeries
-            (
-                x => a * x + b, 0, 30, 5, "Y = ax + b"
-            );
-
-            Model.Axes.Add(X);
-            Model.Axes.Add(Y);
-            Model.Series.Add(LineSeries);
-            Model.Series.Add(FunctionSeries);
-
+        public void SetTrueDistribution(double mean, double precision)
+		{
+            TrueDistribution = Normal.WithMeanPrecision(mean, precision);
+            Model.Series.Clear();
+            Model.Series.Add(new FunctionSeries(TrueDistribution.Density, TrueDistribution.Mean - 20, TrueDistribution.Mean + 20, 0.01, "真の分布"));
             Model.InvalidatePlot(true);
         }
 
-        public class TestData
-        {
-            public TimeSpan Time { get; set; }
-            public double Value { get; set; }
-            public string Tag { get; set; }
+        public IEnumerable<double> Samples(int sampleCount)
+		{
+            return TrueDistribution.Samples().Take(sampleCount).ToArray();
+		}
+
+        private NormalGamma PriorDistribution { get; set; }
+        public void Init(double m, double beta, double a, double b)
+		{
+            PriorDistribution = new NormalGamma(m, beta, a, b);
+            var normal = Normal.WithMeanPrecision(PriorDistribution.MeanMarginal().Mean, PriorDistribution.PrecisionMarginal().Mean);
+            Model.Series.Add(new FunctionSeries(normal.Density, normal.Mean - 20, normal.Mean + 20, 0.01, "学習前（初期）の分布"));
+            Model.InvalidatePlot(true);
+        }
+
+        public void Learn(IEnumerable<double> samples)
+		{
+            var beta = samples.Count() + PriorDistribution.MeanScale;
+            var m = (samples.Sum() + PriorDistribution.MeanScale * PriorDistribution.MeanLocation) / beta;
+            var a = samples.Count() / 2 + PriorDistribution.PrecisionShape;
+            var b = (samples.Sum(target => Math.Pow(target, 2)) + PriorDistribution.MeanScale * Math.Pow(PriorDistribution.MeanLocation, 2) - beta * Math.Pow(m, 2)) / 2 + PriorDistribution.PrecisionInverseScale;
+
+            var posteriorDistribution = new NormalGamma(m, beta, a, b);
+            //var normal = Normal.WithMeanPrecision(posteriorDistribution.MeanMarginal().Mean, posteriorDistribution.PrecisionMarginal().Mean);
+            //Model.Series.Add(new FunctionSeries(normal.Density, normal.Mean - 20, normal.Mean + 20, 0.01, "学習後の分布"));
+
+            var mu = m;
+            var lambda = (beta * a) / ((1 + beta) * b);
+            var nu = 2 * a;
+            // 第2引数が標準偏差なので追加で計算をしている
+            var predictedDistribution = new StudentT(mu, 1 / Math.Sqrt(lambda), nu);
+            Model.Series.Add(new FunctionSeries(predictedDistribution.Density, predictedDistribution.Mean - 20, predictedDistribution.Mean + 20, 0.01, "予測分布"));
+            Model.InvalidatePlot(true);
         }
     }
 }
